1bec9d9be372929423536b4f48b8988ca62373fb
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-11-03 13:30:09 -0400

    breaking html into functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,87 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+// moving header into its own function
+function Header() {
   return (
-    <>
+    // header
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
+  );
+}
+
+// creating new posts moved into own function
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <form>
+        <div>
+          Title: <input type="text" />
+        </div>
+        <div>
+          Body: <input type="text" />
+        </div>
+        <div>
+          Image: <input type="text" />
+        </div>
+        <button type="submit">Post your blog post</button>
+      </form>
+    </div>
+  );
+}
+
+// moved index of posts into its own function
+function PostsIndex() {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Blog post 1</h2>
+        <p>This is the body of my first blog post, it has arms and legs like a real body</p>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVqEyQEa24BuQtSMOjPc24kGBmGMDj4g5p4TDXwdp6vQ&s"
+          alt="stock art of blog post"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR but do i really need to save?!
-          <p></p>i forget how to create space!!
-          <p>i need my space</p>
-        </p>
+      <div>
+        <h2>Blog post 2</h2>
+        <p>This is the body of my second blog post, it has even less substance than the first!</p>
+        <img
+          src="https://thumbs.dreamstime.com/b/hand-drawn-mason-jar-glass-container-empty-kitchen-pot-lid-contour-sketch-hand-drawn-mason-jar-glass-container-empty-kitchen-253357558.jpg"
+          alt="empty jar"
+        />
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2049</p>
+    </footer>
+  );
+}
+
+function Content() {
+  return (
+    <div>
+      <PostsNew />
+      <PostsIndex />
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   );
 }
 
